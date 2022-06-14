e0b7c0a72122b812efcff1af2bef9c8cf005ebaa
Kevin Tervoli <65734031+kevintervoli@users.noreply.github.com>
2022-06-15 00:31:50 +0200

    Added Comments to the code and fixed a bug

--- src/main/java/com/example/datastructure/HelloController.java
+++ src/main/java/com/example/datastructure/HelloController.java
@@ -22,50 +22,57 @@ import java.util.ArrayDeque;
 
 public class HelloController {
     @FXML
+    // declare border pane to update from fxml file
     public BorderPane bp;
     public int i = 0;
+    // declare two array deque to store the data
     ArrayDeque<Sphere> sphereQueue = new ArrayDeque<>();
     ArrayDeque<Label> labelQueue = new ArrayDeque<>();
+    // declare a hbox to insert the sphere and label
     HBox pane = new HBox();
-    Sphere[] spheres = new Sphere[13];
-    Label[] manyLabels = new Label[13];
-
     @FXML
     public void AddFirstButtonClicked(ActionEvent event) throws IOException {
+        // create a material from the image file
         Material mat = new PhongMaterial();
         FileInputStream imgStream = null;
         imgStream = new FileInputStream("Images/color.jpg");
         Image img = new Image(imgStream);
         ((PhongMaterial) mat).setDiffuseMap(img);
+        // set the material to the sphere
         ((PhongMaterial) mat).setSpecularColor(Color.AQUAMARINE);
+        // create a sphere
         Sphere circle = new Sphere();
+        circle.setRadius(70);
+        // create a label
         Label label = new Label();
         label.setFont(new Font("Forte", 18));
-        circle.setRadius(70);
         label.setText(((int) (Math.random() * 13) + ""));
+        // set the material to the sphere
         circle.setMaterial(mat);
         circle.setCullFace(CullFace.BACK);
         circle.setDrawMode(DrawMode.FILL);
-        ;
+        // add them to queue
         sphereQueue.addFirst(circle);
         labelQueue.addFirst(label);
         int i = 0;
         int j = 0;
+        // add them to the pane which will updated after every new addition
         while (i < sphereQueue.size()) {
             StackPane temp = new StackPane();
             temp.getChildren().addAll(sphereQueue.getFirst(), labelQueue.getFirst());
             i++;
             j++;
-            pane.getChildren().add(temp);
+            // add nodes before the first node in the pane
+            pane.getChildren().add(0, temp);
         }
+        // update border pane in order not to show a new pop up window
         bp.setCenter(pane);
     }
 
     public boolean isEmpty() {
-        return spheres.length == 0;
+        return (sphereQueue.size() == 0 && labelQueue.size() == 0);
     }
 
-    @FXML
     public void AddLastButtonClicked() throws FileNotFoundException {
         Material mat = new PhongMaterial();
         FileInputStream imgStream = null;
@@ -95,26 +102,28 @@ public class HelloController {
 
     }
 
-    @FXML
-    public void removeFirst(ActionEvent event) {
+    public void removeFirst() {
+        // remove the first node from the pane
         if (sphereQueue.isEmpty()) {
             return;
         } else if (sphereQueue.size() == 1) {
+            // if the size of the queue is 1, remove the first node from the pane
             pane.getChildren().remove(0);
             sphereQueue.removeFirst();
             labelQueue.removeFirst();
         } else {
+            // if the size of the queue is more than 1, remove the first node from the pane
             pane.getChildren().remove(0);
             sphereQueue.removeFirst();
             labelQueue.removeFirst();
         }
     }
 
-    @FXML
-    public void removeLast(ActionEvent event) {
+    public void removeLast() {
         if (isEmpty()) {
             return;
         } else if (sphereQueue.size() == 1) {
+            // if the size of the queue is 1, remove th last  node from the pane
             pane.getChildren().remove(pane.getChildren().size() - 1);
             sphereQueue.removeLast();
             labelQueue.removeLast();
@@ -124,4 +133,13 @@ public class HelloController {
             labelQueue.removeLast();
         }
     }
+    public void clearQueue() {
+        // it clears all the elements from the queue and the pane
+        sphereQueue.clear();
+        labelQueue.clear();
+        // update pane
+        pane.getChildren().clear();
+
+    }
+
 }
